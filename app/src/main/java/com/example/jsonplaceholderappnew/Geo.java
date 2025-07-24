4edecb5969f5a4058a98be2158fe2004eaaf826a
DanBed666 <daniel@blockether.io>
2025-07-25 00:31:56 +0200

    Add user model class

---
+++ app/src/main/java/com/example/jsonplaceholderappnew/Geo.java
@@ -0,0 +1,13 @@
+package com.example.jsonplaceholderappnew;
+
+public class Geo
+{
+    private String lat;
+    private String lng;
+
+    public Geo(String lat, String lng)
+    {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
