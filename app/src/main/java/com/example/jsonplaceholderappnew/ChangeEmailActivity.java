e5c233aa6351219d4e5d8caebd3ee0ddc294e58f
DanBed666 <daniel@blockether.io>
2025-08-05 21:41:31 +0200

    Add changing user's email address functionality

--- app/src/main/java/com/example/jsonplaceholderappnew/ChangeEmailActivity.java
+++ app/src/main/java/com/example/jsonplaceholderappnew/ChangeEmailActivity.java
@@ -1,22 +1,35 @@
 package com.example.jsonplaceholderappnew;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
 public class ChangeEmailActivity extends AppCompatActivity
 {
     EditText current_mail;
-    EditText password;
+    EditText passwordET;
     EditText new_mail;
     Button send_link;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,7 +45,7 @@ public class ChangeEmailActivity extends AppCompatActivity
         });
 
         current_mail = findViewById(R.id.current_mail);
-        password = findViewById(R.id.password);
+        passwordET = findViewById(R.id.password);
         new_mail = findViewById(R.id.new_mail);
         send_link = findViewById(R.id.send_link);
 
@@ -41,7 +54,54 @@ public class ChangeEmailActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
+                changeEmail();
+            }
+        });
+    }
 
+    public void changeEmail()
+    {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        String email = current_mail.getText().toString();
+        String password = passwordET.getText().toString();
+        AuthCredential authCredential = EmailAuthProvider.getCredential(email, password);
+
+        assert user != null;
+        user.reauthenticate(authCredential).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                if (task.isSuccessful())
+                {
+                    Log.d("AUTH", "Re-authenticated");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String newEmail = new_mail.getText().toString();
+
+                    assert user != null;
+                    user.verifyBeforeUpdateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+                                Log.d("EMAILVER", "Wysłano link weryfikacyjny!");
+                                Toast.makeText(getApplicationContext(), "Wysłano link weryfikacyjny na wskazany adres email!", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Log.e("ERROR", Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()));
+                            }
+                        }
+                    });
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Weryfikacja nieudana! Niepoprawny email lub hasło!", Toast.LENGTH_SHORT).show();
+                    Log.e("ERROR", Objects.requireNonNull(Objects.requireNonNull(task.getException()).getMessage()));
+                }
             }
         });
     }
